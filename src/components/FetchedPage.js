0cc922e721be555602e49aa980517d0e44017aa7
Nishgaur <84866296+Nishgaur@users.noreply.github.com>
2023-05-16 02:00:43 +0530

    Update FetchedPage.js

--- src/components/FetchedPage.js
+++ src/components/FetchedPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WordFrequency from './WordFrequency';
 
 const FetchedPage = ({ location }) => {
   const { data } = location.state || {};
@@ -6,6 +7,7 @@ const FetchedPage = ({ location }) => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-b from-indigo-800 via-violet-900 to-neutral-950 p-4">
       <div className="bg-white text-black p-4 rounded-lg w-full max-w-3xl overflow-x-hidden">
+        <WordFrequency />
         <pre className="whitespace-pre-wrap">{data}</pre>
       </div>
     </div>
